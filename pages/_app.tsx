fa3dbc608d5b7ac5e6f5ff0712e1827e4481d909
JaeHyun Kim <kim19009@byui.edu>
2024-02-01 13:40:10 -0500

    fixing deployment errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import '../app/globals.css';
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import type { AppProps } from 'next/app'
+
 config.autoAddCss = false
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
